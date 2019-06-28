819f749c18754e524a91e3c8f5a889f40f1a44b7
Robert Vincent <robert.vincent@conning.com>
2019-06-28 12:15:54 +0000

    Only download/run terraform if needed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,19 +16,9 @@ class iay(
   IAY::Hash            $hash    = {},
   Stdlib::Absolutepath $workdir = '/var/iay',
 ){
-  include hashicorp::terraform
-
-  file { $workdir:
-    ensure => 'directory',
-    mode   => '0750',
-  }
-  [ 'rehome-redhat' ].each |$file| {
-    file { "${workdir}/${file}":
-      ensure => 'file',
-      before => Exec['terraform apply'],
-      mode   => '0750',
-      source => "puppet:///modules/${module_name}/${file}",
-    }
+  Exec {
+    path    => '/usr/local/bin:/usr/bin',
+    require => Hashicorp::Download['terraform'],
   }
   $hash.each |$k,$v| {
     $content = {
@@ -65,37 +55,54 @@ class iay(
       mode    => '0640',
       path    => "${workdir}/${k}.tf.json",
     }
-  }
-  # Must have at least one provider before calling terraform init.
-  File <| title == 'provider.tf.json' |> ~> Exec['terraform init']
-  Exec {
-    path    => '/usr/local/bin:/usr/bin',
-    require => Hashicorp::Download['terraform'],
-  }
-  exec { 'terraform init':
-    before      => Anchor['iay-terraform-initialized'],
-    cwd         => $workdir,
-    refreshonly => true,
-  }
-  anchor { 'iay-terraform-initialized': }
-  $hash.get('resource', {}).each |IAY::Resource_Type $rtype, IAY::Generic::Hash::Any $rhash| {
-    $rhash.each |IAY::Generic::String1_255 $rname, IAY::Generic::Hash::Any $rval| {
-      exec { "terraform import ${rtype}.${rname} '${rval['import']}'":
-        cwd         => $workdir,
-        notify      => Exec['terraform apply'],
-        refreshonly => true,
-        returns     => [0, 1],
-        require     => Anchor['iay-terraform-initialized'],
-        subscribe   => File['resource.tf.json'],
+    case $k {
+      'provider': {
+        # Must have at least one provider before calling terraform init.
+        exec { 'terraform init':
+          before      => Anchor['iay-terraform-initialized'],
+          cwd         => $workdir,
+          refreshonly => true,
+          subscribe   => File['provider.tf.json'],
+        }
       }
+      'resource': {
+        # Must have at least one resource before invoking terraform at all.
+        include hashicorp::terraform
+        file { $workdir:
+          ensure => 'directory',
+          mode   => '0750',
+        }
+        [ 'rehome-redhat' ].each |$file| {
+          file { "${workdir}/${file}":
+            ensure => 'file',
+            before => Exec['terraform apply'],
+            mode   => '0750',
+            source => "puppet:///modules/${module_name}/${file}",
+          }
+        }
+        anchor { 'iay-terraform-initialized': }
+        $hash.get('resource', {}).each |IAY::Resource_Type $rtype, IAY::Generic::Hash::Any $rhash| {
+          $rhash.each |IAY::Generic::String1_255 $rname, IAY::Generic::Hash::Any $rval| {
+            exec { "terraform import ${rtype}.${rname} '${rval['import']}'":
+              cwd         => $workdir,
+              notify      => Exec['terraform apply'],
+              refreshonly => true,
+              returns     => [0, 1],
+              require     => Anchor['iay-terraform-initialized'],
+              subscribe   => File['resource.tf.json'],
+            }
+          }
+        }
+        exec { 'terraform apply':
+          command     => 'terraform apply -auto-approve',
+          cwd         => $workdir,
+          logoutput   => true,
+          onlyif      => 'test -f resource.tf.json',
+          refreshonly => true,
+          timeout     => 0,
+        }
+      }
+      default: {}
     }
-  }
-  exec { 'terraform apply':
-    command     => 'terraform apply -auto-approve',
-    cwd         => $workdir,
-    logoutput   => true,
-    onlyif      => 'test -f resource.tf.json',
-    refreshonly => true,
-    timeout     => 0,
   }
 }
