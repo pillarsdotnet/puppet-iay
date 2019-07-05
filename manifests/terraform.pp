e3064e06809afdbf2f3f8ad2d35b8b87e408ed3a
Robert Vincent <robert.vincent@conning.com>
2019-07-05 13:22:11 +0000

    Move autoconf.file a little later.

--- manifests/terraform.pp
+++ manifests/terraform.pp
@@ -4,14 +4,6 @@ class iay::terraform {
   include hashicorp::terraform
   $logfile = "${iay::logdir}/iay.log"
 
-  file { '/opt/puppetlabs/puppet/autosign.conf':
-    ensure => 'file',
-    before => Anchor['iay-terraform-configured'],
-    group  => 'puppet',
-    mode   => '0640',
-    owner  => 'puppet',
-  }
-
   exec { 'terraform init':
     before  => Anchor['iay-terraform-initialized'],
     command => 'terraform init >> logfile 2>&1',
@@ -30,6 +22,11 @@ class iay::terraform {
       mode   => '0750',
       source => "puppet:///modules/${module_name}/${file}",
     }
+  }
+  file { '/opt/puppetlabs/puppet/autosign.conf':
+    ensure => 'file',
+    before => Anchor['iay-terraform-imported'],
+    mode   => '0640',
   }
   exec { 'terraform apply':
     command => "terraform apply -auto-approve >>${iay::logfile} 2>&1",
