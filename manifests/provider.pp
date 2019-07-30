8fc49b142228d6e48c2cf7fca441897142b0cc0b
Robert Vincent <robert.vincent@conning.com>
2019-07-30 09:07:42 -0400

    Add missing open-brace.

--- manifests/provider.pp
+++ manifests/provider.pp
@@ -1,7 +1,7 @@
 # Process a provider block.
 
 class iay::provider {
-  if $iay::hash.has_key('provider')
+  if $iay::hash.has_key('provider') {
     $provider = $iay::hash['provider']
     if $provider {
       iay::content { 'provider':
