84508b099efcba229157707f5f88fe3b457ca879
Robert Vincent <robert.vincent@conning.com>
2019-08-09 13:12:05 +0000

    Unwrap provider passwords, too.

--- manifests/provider.pp
+++ manifests/provider.pp
@@ -5,7 +5,28 @@ class iay::provider {
     $provider = $iay::hash['provider']
     if $provider {
       iay::content { 'provider':
-        value => $provider,
+        value => Hash(
+          $provider.map |$rtype, $rhash| {
+            [
+              $rtype,
+              Hash(
+                $rhash.map |$rname, $rval| {
+                  [
+                    $rname,
+                    Hash(
+                      $rval.reduce([]) |$m, $v| {
+                        $v[0] ? {
+                          /password$/ => $m + { $v[0] => unwrap($v[1]) },
+                          default     => $m + $v,
+                        }
+                      }
+                    )
+                  ]
+                }
+              )
+            ]
+          }
+        ),
       }
     }
   }
